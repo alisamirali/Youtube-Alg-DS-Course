77d782842a53d0a4fb9e45c1f668c824f4c278af
Ali Samir <alisamir.eng@gmail.com>
2023-03-19 17:06:41 +0200

    Until 16- Capitalize Algo

---
+++ maxChar/index.js
@@ -0,0 +1,73 @@
+// Return the character most commonly used in the string.
+// --- Examples
+// maxChar("I loveeeeeee noodles") === "e"
+// maxChar("1337") === "3"
+
+function maxChar(str) {
+  // const charCount = {};
+  // let maxCount = 0;
+  // let maxChar = "";
+
+  // for (let char of str) {
+  //   if (charCount[char]) {
+  //     charCount[char]++;
+  //   } else {
+  //     charCount[char] = 1;
+  //   }
+  // }
+
+  // for (let char in charCount) {
+  //   if (charCount[char] > maxCount) {
+  //     maxCount = charCount[char];
+  //     maxChar = char;
+  //   }
+  // }
+
+  // return maxChar;
+
+  const charCount = Object.entries(
+    str.split("").reduce((acc, char) => {
+      acc[char] = acc[char] ? acc[char] + 1 : 1;
+      return acc;
+    }, {})
+  ).reduce((a, b) => (b[1] > a[1] ? b : a));
+
+  return charCount[0];
+}
+
+// _________ _______  _______ _________   _______  _______  _______  _______  _______
+// \__   __/(  ____ \(  ____ \\__   __/  (  ____ \(  ___  )(  ____ \(  ____ \(  ____ \
+//    ) (   | (    \/| (    \/   ) (     | (    \/| (   ) || (    \/| (    \/| (    \/
+//    | |   | (__    | (_____    | |     | |      | (___) || (_____ | (__    | (_____
+//    | |   |  __)   (_____  )   | |     | |      |  ___  |(_____  )|  __)   (_____  )
+//    | |   | (            ) |   | |     | |      | (   ) |      ) || (            ) |
+//    | |   | (____/\/\____) |   | |     | (____/\| )   ( |/\____) || (____/\/\____) |
+//    )_(   (_______/\_______)   )_(     (_______/|/     \|\_______)(_______/\_______)
+//                             ____       _
+//                             |  _ \     | |
+//                             | |_) | ___| | _____      __
+//                             |  _ < / _ \ |/ _ \ \ /\ / /
+//                             | |_) |  __/ | (_) \ V  V /
+//                             |____/ \___|_|\___/ \_/\_/
+//                         ______ ______ ______ ______ ______
+//                         |______|______|______|______|______|
+
+//                          ______ ______ ______ ______ ______
+//                         |______|______|______|______|______|
+
+//                          ______ ______ ______ ______ ______
+//                         |______|______|______|______|______|
+
+mocha.setup("bdd");
+const { assert } = chai;
+
+describe("Max Character", () => {
+  it("maxChar() finds the most frequently used character", () => {
+    assert.equal(maxChar("a"), "a");
+    assert.equal(maxChar("test"), "t");
+    assert.equal(maxChar("I loveeeeee noodles"), "e");
+    assert.equal(maxChar("1337"), "3");
+  });
+});
+
+mocha.run();
